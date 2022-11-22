1a7c9dbb33ccbb203f9ee705e98f07a617519926
LizetMilagros <lcuevas@unsa.edu.pe>
2022-11-22 09:33:25 -0500

    cambios .java

--- app/src/main/java/com/example/x/Login.java
+++ app/src/main/java/com/example/x/Login.java
@@ -60,42 +60,13 @@ public class Login extends Fragment {
                 if (callbackFragment!=null){
                     callbackFragment.changeFragment();
                 }
-                /*String user = username.getText().toString();
-                String pass = password.getText().toString();
-                String repass = confirmPassword.getText().toString();
 
-                if (user.equals("") || pass.equals("") || repass.equals(""))
-                    Toast.makeText(getContext(), "Please enter all the fields", Toast.LENGTH_SHORT).show();
-                else {
-                    if (pass.equals(repass)) {
-                        Boolean checkuser = DB.checkusername(user);
-                        if (checkuser == false) {
-                            Boolean insert = DB.insertData(user, pass);
-                            if (insert == true) {
-                                Toast.makeText(getContext(), "Registered successfully", Toast.LENGTH_SHORT).show();
-                                //Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                                //startActivity(intent);
-                            } else {
-                                Toast.makeText(getContext(), "Registration failed", Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Toast.makeText(getContext(), "User already exists! please sign in", Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        Toast.makeText(getContext(), "Passwords not matching", Toast.LENGTH_SHORT).show();
-                    }
-                }*/
             }
         });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Login log = new Login();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace();
-                fragmentTransaction.commit();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);*/
+
                 String user = username.getText().toString();
                 String pass = password.getText().toString();
 
--- app/src/main/java/com/example/x/MainActivity.java
+++ app/src/main/java/com/example/x/MainActivity.java
@@ -20,65 +20,10 @@ public class MainActivity extends AppCompatActivity implements CallbackFragment 
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
 
-    //EditText username, password, confirmPassword;
-    //Button login, register;
-    //DBusuario DB;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //DB = new DBusuario(this);
-
-        /*username = findViewById(R.id.txtNameUser);
-        password =  findViewById(R.id.txtPassword);
-        confirmPassword =  findViewById(R.id.txtConfirmPassword);
-        login =  findViewById(R.id.btnLogin);
-        register =  findViewById(R.id.btnRegister);
-        DB = new DBusuario(this);
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
-                String repass = confirmPassword.getText().toString();
-
-                if (user.equals("") || pass.equals("") || repass.equals(""))
-                    Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-                else {
-                    if (pass.equals(repass)) {
-                        Boolean checkuser = DB.checkusername(user);
-                        if (checkuser == false) {
-                            Boolean insert = DB.insertData(user, pass);
-                            if (insert == true) {
-                                Toast.makeText(MainActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                                startActivity(intent);
-                            } else {
-                                Toast.makeText(MainActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Toast.makeText(MainActivity.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        Toast.makeText(MainActivity.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
-        /*login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Login log = new Login();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace();
-                fragmentTransaction.commit();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-            }
-        });*/
-
         addFragment();
     }
 
